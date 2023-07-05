c9fe4b93e71ff1348151e93b7b8673215f1bb9e7
Dieter Roelants <dieter.roelants@ap.be>
2023-07-05 21:52:22 +0200

    Add a (for now) hardcoded timetable.
    First step at getting data from the webuntis module in the webview.

--- untapped.py
+++ untapped.py
@@ -1,7 +1,22 @@
+import datetime
+
 try:
     import webview
 except ImportError:
     exit('install the pywebview package')
 
-webview.create_window('Untapped', url='./untapped.html')
-webview.start(debug=False)
+import py_ap_untis
+
+def getTTable():
+    s = py_ap_untis.get_session()
+    day1 = datetime.date(2023, 9, 18)
+    tt = s.timetable(start=day1, end=(day1 + datetime.timedelta(days=4)), teacher=3817)
+    for e in tt:
+        day = (e.start.date() - day1).days + 1
+        e._data['day'] = day
+    return [e._data for e in tt]
+
+
+ui = webview.create_window('Untapped', url='./untapped.html')
+ui.expose(getTTable)
+webview.start(debug=True)
