7e22c6f6c8cf865a72d0838c46a797d210383584
Dieter Roelants <dieter.roelants@ap.be>
2023-07-06 11:36:50 +0200

    Make untislogin fuction work.

--- untapped.py
+++ untapped.py
@@ -7,17 +7,26 @@ except ImportError:
 
 import py_ap_untis
 
+untis_session = None
+
 def untisLogin(username, passwd):
-    return True
+    global untis_session
+    py_ap_untis._user = username
+    py_ap_untis._password = passwd
+    untis_session = py_ap_untis.get_session()
+    return bool(untis_session)
 
 def getTeacherTable(teacher, tbldate):
     '''
     Return the timetable for the given teacher (numeric id) and the tbldate
     which needs to be in isoformat.
     '''
-    s = py_ap_untis.get_session()
+    #s = py_ap_untis.get_session()
+    global untis_session
     day1 = datetime.date.fromisoformat(tbldate)
-    tt = s.timetable(start=day1, end=(day1 + datetime.timedelta(days=4)), teacher=teacher)
+    tt = untis_session.timetable(start=day1,
+                                 end=(day1 + datetime.timedelta(days=4)),
+                                 teacher=teacher)
     for e in tt:
         day = (e.start.date() - day1).days + 1
         e._data['day'] = day
